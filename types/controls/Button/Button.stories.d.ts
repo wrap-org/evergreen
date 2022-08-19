f08f26fdb2323fc06736a116beb19863c42a8878
Dan Webb <dan@etch.co>
2022-08-19 12:58:39 +0100

    fix: generated d.ts files included in repo

--- types/controls/Button/Button.stories.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-import { Meta } from '@storybook/react/types-6-0';
-declare const _default: Meta<import("@storybook/react/types-6-0").Args>;
-export default _default;
-export declare const Types: () => JSX.Element;
-export declare const Block: () => JSX.Element;
--- types/index.d.ts
+++
@@ -1,3 +0,0 @@
-import Icon from 'components/content/Icon/Icon';
-import Button from 'components/controls/Button/Button';
-export { Icon, Button };
