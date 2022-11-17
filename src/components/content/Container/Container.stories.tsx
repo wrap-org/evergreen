44553ebcd6cac20cd3aa2febfdf0ea0e7a5ef8ca
Gavyn McKenzie <gavyn@etch.co>
2022-11-17 15:03:43 +0000

    feat: container and container svg components

---
+++ src/components/content/Container/Container.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Meta } from '@storybook/react/types-6-0';
+
+import Container from './Container';
+
+export default {
+  title: 'Content/Container',
+  component: Container,
+} as Meta;
+
+export const Default = () => (
+  <Container
+    name="Wheeled Bin"
+    displayName="Wheeled Bin (140L+)"
+    bodyColour="#3cb848"
+  />
+);
