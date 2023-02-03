e1310fa9839e74a7096baa5c348f1cea2bc24bd1
Gavyn McKenzie <gavyn@etch.co>
2023-02-03 14:41:45 +0000

    feat: auto animate lists

--- src/components/composition/List/List.stories.tsx
+++ src/components/composition/List/List.stories.tsx
@@ -1,9 +1,12 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import React from 'react';
+import React, { useState } from 'react';
 
 import List from './List';
 
+import Grid from 'components/composition/Grid/Grid';
+import Spacing from 'components/composition/Spacing/Spacing';
 import Icon from 'components/content/Icon/Icon';
+import Button from 'components/controls/Button/Button';
 
 export default {
   title: 'Composition/List',
@@ -26,3 +29,40 @@ const Template: ComponentStory<typeof List> = (args) => (
 export const Default = Template.bind({});
 
 Default.storyName = 'List';
+
+export const Animated: ComponentStory<typeof List> = (args) => {
+  const [items, setItems] = useState([1, 2, 3, 4, 5, 6]);
+
+  const remove = () => {
+    const i = Math.floor(Math.random() * items.length);
+    setItems(items.filter((val, key) => key !== i));
+  };
+
+  const add = () => {
+    const i = Math.floor(Math.random() * 100);
+    setItems([...items, i]);
+  };
+
+  return (
+    <>
+      <List {...args}>
+        {items.map((item) => (
+          <List.Item key={item} icon={<Icon icon="favorite" />}>
+            Icon List Item
+          </List.Item>
+        ))}
+      </List>
+      <Spacing bottom="md" />
+      <Grid>
+        <Grid.Item>
+          <Button onClick={remove}>Remove item</Button>
+        </Grid.Item>
+        <Grid.Item>
+          <Button type="primary" onClick={add}>
+            Add item
+          </Button>
+        </Grid.Item>
+      </Grid>
+    </>
+  );
+};
