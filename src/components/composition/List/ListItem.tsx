29e6c77290d32a5adb28d3b154623c744080c13a
Dan Webb <dan@etch.co>
2024-01-09 17:00:28 +0000

    fix: list item width

--- src/components/composition/List/ListItem.tsx
+++ src/components/composition/List/ListItem.tsx
@@ -11,7 +11,7 @@ export interface ListItemProps {
 const Item = ({ icon, children, ...props }: Readonly<ListItemProps>) => (
   <li className={styles.list__item} {...props}>
     {icon ? <div className={styles['list__item-icon']}>{icon}</div> : null}
-    {children}
+    <div className={styles['list__item-content']}>{children}</div>
   </li>
 );
 
