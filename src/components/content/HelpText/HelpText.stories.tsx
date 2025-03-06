2c1543ff4db53ff3380db14a1bbf1efaace189ca
Annie Seaward <annie@etch.co>
2025-03-06 09:33:05 +0000

    docs: improve help text component stories

--- src/components/content/HelpText/HelpText.stories.tsx
+++ src/components/content/HelpText/HelpText.stories.tsx
@@ -40,15 +40,75 @@ export const HelpText: StoryFn = (args) => (
       />
     </evg-input>
     <evg-help-text id="my-input-helptext" type={args.type}>
-      <div>
-        {args.type === 'positive' && 'Yay, it worked!'}
-        {args.type === 'negative' && 'There was a problem'}
-        {args.type === 'neutral' && 'Some help text for this input'}
-      </div>
+      <div>Some help text for this input</div>
     </evg-help-text>
   </FormGroup>
 );
 
 HelpText.args = {
   type: 'neutral',
+};
+
+export const HelpTextTypes: StoryFn = () => (
+  <evg-grid direction="column">
+    <evg-grid-item>
+      <evg-help-text id="my-input-helptext" type={'neutral'}>
+        <div>Some help text for this input</div>
+      </evg-help-text>
+    </evg-grid-item>
+    <evg-grid-item>
+      <evg-help-text id="my-input-helptext" type={'positive'}>
+        <div>Yay, it worked!</div>
+      </evg-help-text>
+    </evg-grid-item>
+    <evg-grid-item>
+      <evg-help-text id="my-input-helptext" type={'negative'}>
+        <div>There was a problem</div>
+      </evg-help-text>
+    </evg-grid-item>
+  </evg-grid>
+);
+
+export const HelpTextElement: StoryFn = () => (
+  <evg-grid direction="column">
+    <evg-grid-item>
+      <FormGroup>
+        <Label htmlFor="my-input1">Input label</Label>
+        <evg-input>
+          <input
+            id="my-input1"
+            name="my-input1"
+            aria-describedby="my-input-helptext1"
+          />
+        </evg-input>
+        <evg-help-text id="my-input-helptext1">
+          <div>Some help text in a div element</div>
+        </evg-help-text>
+      </FormGroup>
+    </evg-grid-item>
+    <evg-grid-item>
+      <FormGroup>
+        <Label htmlFor="my-input2">Input label</Label>
+        <evg-input>
+          <input
+            id="my-input2"
+            name="my-input2"
+            aria-describedby="my-input-helptext2"
+          />
+        </evg-input>
+        <evg-help-text id="my-input-helptext2">
+          <span>Some help text in a span element</span>
+        </evg-help-text>
+      </FormGroup>
+    </evg-grid-item>
+  </evg-grid>
+);
+
+HelpTextElement.parameters = {
+  docs: {
+    description: {
+      story:
+        'Help text can wrap a div or a span element to control whether it is used inline or as a block element.',
+    },
+  },
 };
