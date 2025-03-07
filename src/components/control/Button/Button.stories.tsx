d4e964430915ef8a8e7010b3db094cbb39bd4cb8
Gavyn McKenzie <gavyn@etch.co>
2025-03-07 11:52:46 +0000

    docs: a11y text on icon buttons

--- src/components/control/Button/Button.stories.tsx
+++ src/components/control/Button/Button.stories.tsx
@@ -90,7 +90,7 @@ export const IconOnlyButton: StoryFn = () => (
   <evg-grid wrap="wrap" align-items="center">
     <evg-grid-item>
       <evg-button width="square">
-        <button type="button">
+        <button type="button" aria-label="Button text">
           <svg fill="none" viewBox="0 0 24 24" height="24" width="24">
             <path
               d="M2 6C2 4.89543 2.89543 4 4 4H20C21.1046 4 22 4.89543 22 6V18C22 19.1046 21.1046 20 20 20H4C2.89543 20 2 19.1046 2 18V6ZM5.51859 6L12 11.6712L18.4814 6H5.51859ZM20 7.32877L12.6585 13.7526C12.2815 14.0825 11.7185 14.0825 11.3415 13.7526L4 7.32877V18H20V7.32877Z"
@@ -102,7 +102,7 @@ export const IconOnlyButton: StoryFn = () => (
     </evg-grid-item>
     <evg-grid-item>
       <evg-button variant="text">
-        <button type="button">
+        <button type="button" aria-label="Button text">
           <svg fill="none" viewBox="0 0 24 24" height="24" width="24">
             <path
               d="M2 6C2 4.89543 2.89543 4 4 4H20C21.1046 4 22 4.89543 22 6V18C22 19.1046 21.1046 20 20 20H4C2.89543 20 2 19.1046 2 18V6ZM5.51859 6L12 11.6712L18.4814 6H5.51859ZM20 7.32877L12.6585 13.7526C12.2815 14.0825 11.7185 14.0825 11.3415 13.7526L4 7.32877V18H20V7.32877Z"
