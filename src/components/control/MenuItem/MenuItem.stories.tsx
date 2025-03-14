1814924595d7ad315975a32de588a7c6fe23ac8b
Gavyn McKenzie <gavyn@etch.co>
2025-03-14 06:59:45 +0000

    feat: menu item theming

--- src/components/control/MenuItem/MenuItem.stories.tsx
+++ src/components/control/MenuItem/MenuItem.stories.tsx
@@ -1,6 +1,8 @@
 import { StoryFn } from '@storybook/react';
 import React from 'react';
 
+import { themes } from '@/lib/theme';
+
 export default {
   component: 'evg-menu-item',
   parameters: {
@@ -92,3 +94,33 @@ export const WithDescriptionAndBadges: StoryFn = (props) => (
     </evg-menu-item>
   </>
 );
+
+export const Theming: StoryFn = (props) => (
+  <>
+    {themes.map((theme) => (
+      <evg-section padding="fluid" class={`evg-theme-${theme}`} key={theme}>
+        <evg-wrap size="lg">
+          <evg-menu-item {...props}>
+            <button type="button">
+              <evg-icon icon="document-check" />
+              <evg-menu-item-content>
+                Menu item title
+                <p>Menu item description</p>
+                <evg-meta>
+                  <evg-badge type="info">Portal</evg-badge>
+                  <evg-badge type="positive">Locator</evg-badge>
+                  <evg-badge type="light">API</evg-badge>
+                </evg-meta>
+              </evg-menu-item-content>
+              <evg-icon icon="arrow-right" />
+            </button>
+          </evg-menu-item>
+        </evg-wrap>
+      </evg-section>
+    ))}
+  </>
+);
+
+Theming.parameters = {
+  layout: 'fullscreen',
+};
