92eccdd7cb3776592582500866271f4f8fed41fb
Annie Seaward <annie@etch.co>
2025-03-18 23:09:55 +0000

    feat!: remove react loading components

--- src/components/react/content/LoadingSpinner/LoadingSpinner.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import styles from './LoadingSpinner.module.scss';
-
-export default function LoadingSpinner() {
-  return <div className={styles['loading-spinner']}></div>;
-}
