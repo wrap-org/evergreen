b9a76e7ec8e323976a8385da50095202dbc2ce3f
Annie Seaward <annie@etch.co>
2025-03-19 09:24:14 +0000

    test: disable a11y test for loading-text

--- src/components/content/LoadingText/LoadingText.stories.tsx
+++ src/components/content/LoadingText/LoadingText.stories.tsx
@@ -12,12 +12,18 @@ export default {
           'Used as a loading placeholder before the content is available.',
       },
     },
+    a11y: {
+      config: {
+        // Getting a false positive: h1 element correctly has no findable text content
+        rules: [{ id: 'empty-heading', enabled: false }],
+      },
+    },
   },
 } satisfies Meta;
 
 export const LoadingText = () => (
   <>
-    <h1>
+    <h1 className="evg-spacing-bottom-sm">
       <evg-loading-text>Loading title</evg-loading-text>
     </h1>
     <p>
