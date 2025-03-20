8f936994085ed0e3aff9350c056633ee17ad1e69
Annie Seaward <annie@etch.co>
2025-03-20 06:57:53 +0000

    docs: reformate storybook description for loading spinner

--- src/components/content/LoadingSpinner/LoadingSpinner.stories.tsx
+++ src/components/content/LoadingSpinner/LoadingSpinner.stories.tsx
@@ -3,15 +3,18 @@ import React from 'react';
 
 import './LoadingSpinner';
 
+const description = `
+Used to communicate content is loading before the it is available.
+
+Using a loading spinner should always be a last resort. The "spinning wheel of death" is a known term in dictionary.com because of the dread it inspires in users who know that <code>loading spinner == slow.</code>.
+Instead of using a loading spinner, consider using skeleton screens made from <code><evg-loading-button></code>, <code><evg-loading-img></code> or <code><evg-loading-text></code>.
+`;
+
 export default {
   title: 'Components/Content/Loading Spinner',
   parameters: {
     docs: {
-      description: {
-        component: `Used to communicate content is loading before the it is available.
-          Using a loading spinner should always be a last resort. The "spinning wheel of death"" is a known term in dictionary.com because of the dread it inspires in users who know that <code>loading spinner == slow.</code>.
-          Instead of using a loading spinner, consider using skeleton screens made from <code><evg-loading-button></code>, <code><evg-loading-img></code> or <code><evg-loading-text></code>.`,
-      },
+      description: { component: description },
     },
   },
 } satisfies Meta;
