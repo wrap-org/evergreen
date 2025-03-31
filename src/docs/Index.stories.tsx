0c46bddfa64902d3e12d084267a2f58bd17d525b
Annie Seaward <annie@etch.co>
2025-03-31 21:23:09 +0100

    docs: manually override a11y issue on homepage

--- src/docs/Index.stories.tsx
+++ src/docs/Index.stories.tsx
@@ -4,6 +4,20 @@ export default {
   title: 'Home',
   parameters: {
     layout: 'fullscreen',
+    a11y: {
+      /* Prevent a11y tests displaying on homepage */
+      config: {
+        rules: [
+          {
+            id: 'color-contrast',
+            selector: '.evg-text-size-heading-lg',
+            enabled: false,
+            reason:
+              'Text over image with sufficient contrast verified manually',
+          },
+        ],
+      },
+    },
   },
   tags: ['!autodocs'],
 };
