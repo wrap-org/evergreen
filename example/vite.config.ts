5f505468bd34b1ae8458fde34aaaaee281fb1627
Dan Webb <dan@etch.co>
2025-08-04 11:29:58 +0100

    docs: remove unecessary vite server config

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -5,11 +5,4 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   // Set the public directory to the assets folder in the parent src directory
   publicDir: path.resolve(__dirname, '../src/assets'),
-  // Ensure the server serves files correctly
-  server: {
-    fs: {
-      // Allow serving assets from parent directories
-      allow: ['..'],
-    },
-  },
 });
