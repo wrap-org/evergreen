22703f4215740a71d1dbe216f312d148497abd53
Dan Webb <dan@etch.co>
2025-08-04 15:41:50 +0100

    docs: add comment about public dir

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -3,6 +3,9 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // Set the public directory to the assets folder in the parent src directory
-  publicDir: path.resolve(__dirname, '../src/assets'),
+  // Set the public directory to the evergreen assets folder.
+  publicDir: path.resolve(
+    __dirname,
+    '../src/assets', // this would usually be node_modules/@wrap.ngo/evergreen/src/assets
+  ),
 });
