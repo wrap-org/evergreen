7a7ef0d55b75d9a52bb9db9703308840ac363c17
Gavyn McKenzie <gavyn@etch.co>
2026-07-13 17:39:41 +0100

    fix: evg-icon and custom svg in input affix to same size

--- src/components/control/Input/Input.stories.tsx
+++ src/components/control/Input/Input.stories.tsx
@@ -73,12 +73,12 @@ export const Affix: StoryFn = () => (
       <evg-grid-item>
         <evg-input>
           <evg-icon icon="email" />
-          <input value="prefix" />
+          <input value="Prefix" />
         </evg-input>
       </evg-grid-item>
       <evg-grid-item>
         <evg-input>
-          <input value="suffix" />
+          <input value="Suffix" />
           <evg-icon icon="email" />
         </evg-input>
       </evg-grid-item>
@@ -95,12 +95,12 @@ export const Affix: StoryFn = () => (
               <evg-icon icon="email" />
             </button>
           </evg-button>
-          <input value="prefix" />
+          <input value="Prefix" />
         </evg-input>
       </evg-grid-item>
       <evg-grid-item>
         <evg-input>
-          <input value="suffix" />
+          <input value="Suffix" />
           <evg-button variant="ghost" width="square">
             <button type="button" aria-label="Button text">
               <evg-icon icon="email" />
@@ -116,60 +116,24 @@ export const Affix: StoryFn = () => (
     <evg-grid wrap="wrap" class="evg-spacing-md">
       <evg-grid-item>
         <evg-input>
-          <svg fill="none" viewBox="0 0 24 24" height="24" width="24">
+          <svg fill="none" viewBox="0 0 24 24" height="20" width="20">
             <path
               d="M2 6C2 4.89543 2.89543 4 4 4H20C21.1046 4 22 4.89543 22 6V18C22 19.1046 21.1046 20 20 20H4C2.89543 20 2 19.1046 2 18V6ZM5.51859 6L12 11.6712L18.4814 6H5.51859ZM20 7.32877L12.6585 13.7526C12.2815 14.0825 11.7185 14.0825 11.3415 13.7526L4 7.32877V18H20V7.32877Z"
               fill="currentColor"
             ></path>
           </svg>
-          <input value="prefix" />
+          <input value="Prefix" />
         </evg-input>
       </evg-grid-item>
       <evg-grid-item>
         <evg-input>
-          <input value="suffix" />
-          <svg fill="none" viewBox="0 0 24 24" height="24" width="24">
+          <input value="Suffix" />
+          <svg fill="none" viewBox="0 0 24 24" height="20" width="20">
             <path
               d="M2 6C2 4.89543 2.89543 4 4 4H20C21.1046 4 22 4.89543 22 6V18C22 19.1046 21.1046 20 20 20H4C2.89543 20 2 19.1046 2 18V6ZM5.51859 6L12 11.6712L18.4814 6H5.51859ZM20 7.32877L12.6585 13.7526C12.2815 14.0825 11.7185 14.0825 11.3415 13.7526L4 7.32877V18H20V7.32877Z"
               fill="currentColor"
             ></path>
           </svg>
-        </evg-input>
-      </evg-grid-item>
-    </evg-grid>
-
-    <h2 className="evg-text-size-heading-sm evg-spacing-sm">
-      Affix icon with custom svg button
-    </h2>
-    <evg-grid wrap="wrap" class="evg-spacing-md">
-      <evg-grid-item>
-        <evg-input>
-          <evg-button variant="ghost" width="square">
-            <button type="button" aria-label="Button text">
-              <svg fill="none" viewBox="0 0 24 24" height="24" width="24">
-                <path
-                  d="M2 6C2 4.89543 2.89543 4 4 4H20C21.1046 4 22 4.89543 22 6V18C22 19.1046 21.1046 20 20 20H4C2.89543 20 2 19.1046 2 18V6ZM5.51859 6L12 11.6712L18.4814 6H5.51859ZM20 7.32877L12.6585 13.7526C12.2815 14.0825 11.7185 14.0825 11.3415 13.7526L4 7.32877V18H20V7.32877Z"
-                  fill="currentColor"
-                ></path>
-              </svg>
-            </button>
-          </evg-button>
-          <input value="prefix" />
-        </evg-input>
-      </evg-grid-item>
-      <evg-grid-item>
-        <evg-input>
-          <input value="suffix" />
-          <evg-button variant="ghost" width="square">
-            <button type="button" aria-label="Button text">
-              <svg fill="none" viewBox="0 0 24 24" height="24" width="24">
-                <path
-                  d="M2 6C2 4.89543 2.89543 4 4 4H20C21.1046 4 22 4.89543 22 6V18C22 19.1046 21.1046 20 20 20H4C2.89543 20 2 19.1046 2 18V6ZM5.51859 6L12 11.6712L18.4814 6H5.51859ZM20 7.32877L12.6585 13.7526C12.2815 14.0825 11.7185 14.0825 11.3415 13.7526L4 7.32877V18H20V7.32877Z"
-                  fill="currentColor"
-                ></path>
-              </svg>
-            </button>
-          </evg-button>
         </evg-input>
       </evg-grid-item>
     </evg-grid>
